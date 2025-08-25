98b7fcfb114cc2457473e33ef2ca370977f8d3d8
m-7670206-creator <m-7670206@moe-dl.edu.my>
2025-08-25 12:21:38 +0800

    Add files via upload

---
+++ double_num.py
@@ -0,0 +1,17 @@
+def main():
+    nums = [2, 7, 11, 15]
+    target = 9
+    n = len(nums)
+
+    dictionary = {}
+
+    for i in range(n):
+        match = target - nums[i]
+        try:
+            print(i, nums.index(match))
+            break
+        except ValueError:
+            pass
+
+
+main()
